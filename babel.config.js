009674a356463c61097663ff746a6f5f1f465899
JustReallyBoredTryingStuffOut <m.ahmedkhan1708@gmail.com>
2025-06-30 19:57:21 +0200

    Fix React version conflict and update Babel configuration - Downgraded React from 19.0.0 to 18.2.0 to resolve dependency conflicts - Updated Babel plugins to use newer transform plugins instead of deprecated proposal plugins - Cleaned iOS build cache - Metro server now running successfully on port 8081 - All HealthKit permissions and entitlements properly configured

--- babel.config.js
+++ babel.config.js
@@ -11,15 +11,18 @@ module.exports = function (api) {
       '@babel/plugin-transform-export-namespace-from',
       '@babel/plugin-proposal-export-default-from',
       ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      '@babel/plugin-transform-optional-chaining',
+      '@babel/plugin-transform-nullish-coalescing-operator',
       '@babel/plugin-transform-logical-assignment-operators',
       '@babel/plugin-proposal-function-bind',
       '@babel/plugin-proposal-partial-application',
       ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
       '@babel/plugin-proposal-record-and-tuple',
-      '@babel/plugin-proposal-throw-expressions'
+      '@babel/plugin-proposal-throw-expressions',
+      '@babel/plugin-transform-runtime',
+      '@babel/preset-typescript',
+      'transform-remove-console'
     ],
     env: {
       production: {
